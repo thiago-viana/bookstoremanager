4051845d2273349d62a56ad9bd69f2d2e99e556f
Thiago <thiago.f.v.silva@gmail.com>
2022-01-02 00:57:50 -0300

    Created model mappings

---
+++ src/main/java/com/thiagoferreira/bookstoremanager/publisher/entity/Publisher.java
@@ -0,0 +1,29 @@
+package com.thiagoferreira.bookstoremanager.publisher.entity;
+
+import com.thiagoferreira.bookstoremanager.book.entity.Book;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 0)
+    private String code;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDate foundationDate;
+
+    @OneToMany(mappedBy = "publisher", fetch = FetchType.LAZY)
+    private List<Book> books;
+}
---
+++ src/main/java/com/thiagoferreira/bookstoremanager/user/enums/Gender.java
@@ -0,0 +1,14 @@
+package com.thiagoferreira.bookstoremanager.user.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Gender {
+
+    MALE("Male"),
+    FEMALE("Female");
+
+    private String description;
+}
